6970f1845ddf7df8ffa465f3e9d98b7cf9ac69cc
chernik1 <vvvvf-vvv@mail.ru>
2023-10-19 23:06:39 +0300

    add parser

--- parser/test.py
+++ parser/test.py
@@ -1,27 +1,33 @@
 from playwright.sync_api import Playwright, sync_playwright, expect, Page
 from settings import today, yesterday, keywords
-def run(playwright: Playwright) -> None:
+from bs4 import BeautifulSoup
+
+list_of_keywords = []
+
+def run(playwright: Playwright, keyword: str) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://zakupki.butb.by/auctions/reestrauctions.html")
     page.get_by_role("link", name="Раскрыть форму поиска").click()
     page.locator("input[name=\"fra\\:j_idt174\"]").click()
-    page.locator("input[name=\"fra\\:j_idt174\"]").press("CapsLock")
-    page.locator("input[name=\"fra\\:j_idt174\"]").fill("ЭВМ")
-    page.locator("input[name=\"fra\\:j_idt174\"]").press("CapsLock")
+    page.locator("input[name=\"fra\\:j_idt174\"]").fill(keyword)
     page.locator("input[name=\"fra\\:date1\"]").click()
-    page.locator("input[name=\"fra\\:date1\"]").fill("18.10.2023")
+    page.locator("input[name=\"fra\\:date1\"]").fill(yesterday)
     page.locator("input[name=\"fra\\:date2\"]").click()
-    page.locator("input[name=\"fra\\:date2\"]").fill("19.10.2023")
+    page.locator("input[name=\"fra\\:date2\"]").fill(today)
     page.get_by_role("button", name="Искать").click()
-    page.get_by_role("link", name="ПЭВМ").click()
+
+    soup = BeautifulSoup(page.content(), "html.parser")
+    a = soup.find(id='fra:auctionList:tbody').find_all('a')
 
     # ---------------------
     context.close()
     browser.close()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+for keyword in keywords:
+    with sync_playwright() as playwright:
+        run(playwright, keyword)
+
 
