24571ddae2f55a1d8978a1e97036cbf02e77e9ce
chernik1 <vvvvf-vvv@mail.ru>
2023-11-07 14:03:36 +0300

    add template for ai

--- main/ai_assistent/run.py
+++ main/ai_assistent/run.py
@@ -1,18 +1,25 @@
-# from .settings import combined_request
-# import g4f
-#
-# def run_ai(data):
-#     for i in range(4, 15):
-#         element = data[i]
-#         print(element.price, element.main_name_purchase, element.location)
-#         request = str(combined_request) + element.location + element.main_name_purchase + element.price + 'Твой ответ только + или -(без пояснений)'
-#         # Set the provider
-#         response = g4f.ChatCompletion.create(
-#             model="gpt-3.5-turbo",
-#             messages=[{"role": "user", "content": request}],
-#             provider=g4f.Provider.FreeGpt,
-#             stream=True,
-#         )
-#         for chunk in response:
-#             print(chunk)
-#
+from .settings import combined_request
+import g4f
+
+def run_ai(data):
+    for i in range(4, 15):
+        element = data[i]
+        print(element.price, element.main_name_purchase, element.location)
+        request = combined_request + element.location + ' + ' + element.main_name_purchase + ' + ' + element.price + 'Ответ 1 символ + или -'
+        # Set the provider
+        print(request)
+        try:
+            response = g4f.ChatCompletion.create(
+                model="gpt-4",
+                messages=[{"role": "user", "content": request}],
+                provider=g4f.Provider.Bing,
+                stream=True,
+            )
+        except Exception as e:
+            response = ['None']
+        s = ''
+        for i in response:
+            s += i
+        print(s)
+
+
