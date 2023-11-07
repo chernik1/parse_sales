5c3b11477bef600048b606bd2fdda0277497012e
chernik1 <vvvvf-vvv@mail.ru>
2023-11-07 14:14:12 +0300

    add template for ai

--- main/ai_assistent/run.py
+++ main/ai_assistent/run.py
@@ -1,5 +1,17 @@
 from .settings import combined_request
 import g4f
+from g4f.Provider import (
+    AItianhu,
+    Aichat,
+    Bard,
+    Bing,
+    ChatBase,
+    ChatgptAi,
+    OpenaiChat,
+    Vercel,
+    You,
+    Yqcloud,
+)
 
 def run_ai(data):
     for i in range(4, 15):
@@ -10,9 +22,9 @@ def run_ai(data):
         print(request)
         try:
             response = g4f.ChatCompletion.create(
-                model="gpt-4",
+                model="gpt-3.5-turbo",
                 messages=[{"role": "user", "content": request}],
-                provider=g4f.Provider.Bing,
+                provider=g4f.Provider.ChatgptAi,
                 stream=True,
             )
         except Exception as e:
