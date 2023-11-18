d664a051f91293627b80a7450cd4993e2b439bdc
chernik1 <vvvvf-vvv@mail.ru>
2023-11-18 23:41:07 +0300

    ai work

--- main/ai_assistent/run_zaku.py
+++ main/ai_assistent/run_zaku.py
@@ -6,14 +6,17 @@ from .validate import is_validate
 
 db = []
 
+import asyncio
+
 async def run_ai(data):
     new_db_zaku = []
+    tasks = []
+
     for index, element in enumerate(data):
         print(element.price, element.main_name_purchase, element.location)
-        promt = combined_request + element.location + ' , ' + element.main_name_purchase + ' , ' + element.price + "Напиши свой ответ без объяснений в виде **+** или **-** или **A**."
+        promt = combined_request + element.location + ' , ' + element.main_name_purchase + ' , ' + element.price + "Напиши свой ответ БЕЗ ОБЪЯСНЕНИЙ в виде **+** или **-** или **A**."
 
         if not is_validate(element):
-
             new_db_zaku.append(
                 {
                     'keyword': element.keyword,
@@ -27,23 +30,19 @@ async def run_ai(data):
                     'forecast': 'Проверка не пройдена',
                 }
             )
-
             continue
 
         print(promt)
-        try:
-            response = await g4f.ChatCompletion.create_async(
-                model="gpt-4",
-                provider=g4f.Provider.Bing,
-                messages=[{"role": "user", "content": promt}],
-            )
-        except Exception as e:
-            print(e)
-            response = ['None']
+        task = asyncio.create_task(make_request(promt))
+        tasks.append(task)
+
+    responses = await asyncio.gather(*tasks)
+
+    for index, response in enumerate(responses):
+        element = data[index]
         s = ''
         for i in response:
             s += i
-        print(s)
         s = s.split()
         if '**+**' in s or '+' in s:
             forecast = 'Купить'
@@ -53,6 +52,7 @@ async def run_ai(data):
             forecast = 'Не знаю'
         else:
             forecast = 'Неопределён'
+        print(s)
 
         new_db_zaku.append(
             {
@@ -67,8 +67,20 @@ async def run_ai(data):
                 'forecast': forecast,
             }
         )
+
     return new_db_zaku
 
+async def make_request(promt):
+    try:
+        response = await g4f.ChatCompletion.create_async(
+            model=g4f.models.default,
+            provider=g4f.Provider.Bing,
+            messages=[{"role": "user", "content": promt}],
+        )
+    except Exception as e:
+        print(e)
+        response = ['None']
+    return response
 
 def run_programm(data):
     global db
