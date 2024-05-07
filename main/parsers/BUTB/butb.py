2f4995ae5c26412211686b22dde84b540f5377b8
chernik1 <vvvvf-vvv@mail.ru>
2024-05-07 20:59:03 +0300

    fix bag

--- main/parsers/BUTB/butb.py
+++ main/parsers/BUTB/butb.py
@@ -50,55 +50,74 @@ async def check_page(page: Page) -> tuple[Page, list]:
 
 async def parse_a_href(page: Page) -> tuple[Page, list]:
     """Функция для парсинга ссылок на страницы."""
+    async def index_span(elements, keyword):
+        index = 0
+        if keyword in ('Полное наименование', 'УНП'):
+            elements = elements[23:]
+            index = 23
+        for element in elements:
+            if element.text == keyword:
+                break
+            index += 1
+        return index
+
     list_of_objects = []
 
     html_content = await page.content()
     soup = BeautifulSoup(html_content, 'html.parser')
 
     register = soup(text='Регистрационный номер')
-    register_tr = register[0].find_parent('tr')
+    register_tr = register[0].find_parent('div')
     register_all_span = register_tr.find_all('span')
-    register_text = register_all_span[1].text
+    register_index = await index_span(register_all_span, 'Регистрационный номер')
+    register_text = register_all_span[register_index + 1].text
     list_of_objects.append(register_text)
 
     name_company = soup(text='Полное наименование')
-    name_company_tr = name_company[1].find_parent('tr')
+    name_company_tr = name_company[1].find_parent('div')
     name_company_all_span = name_company_tr.find_all('span')
-    name_company_text = name_company_all_span[1].text
+    name_company_index = await index_span(name_company_all_span, 'Полное наименование')
+    name_company_text = name_company_all_span[name_company_index + 1].text
     list_of_objects.append(name_company_text)
 
     date = soup(text='Дата размещения приглашения')
-    date_tr = date[0].find_parent('tr')
+    date_tr = date[0].find_parent('div')
     date_all_span = date_tr.find_all('span')
-    date_text = date_all_span[1].text
+    date_index = await index_span(date_all_span, 'Дата размещения приглашения')
+    date_text = date_all_span[date_index + 1].text
     list_of_objects.append(date_text)
 
     name_purchase = soup(text='Наименование закупки')
-    name_purchase_tr = name_purchase[0].find_parent('tr')
+    name_purchase_tr = name_purchase[0].find_parent('div')
     name_purchase_all_span = name_purchase_tr.find_all('span')
-    name_purchase_text = name_purchase_all_span[1].text
+    name_purchase_index = await index_span(name_purchase_all_span, 'Наименование закупки')
+    name_purchase_text = name_purchase_all_span[name_purchase_index + 1].text
     list_of_objects.append(name_purchase_text)
 
-    price = soup(text='Ориентировочная стоимость закупки')
+    price = soup(text='Предельная cтоимость закупки')
+    text_price = 'Предельная cтоимость закупки'
     if len(price) == 0:
-        list_of_objects.append('Неизвестно')
-    else:
-        price_tr = price[0].find_parent('tr')
-        price_all_span = price_tr.find_all('span')
-        price_text = price_all_span[1].text
-        price_text = price_text.replace("\xa0", "").replace("\\xa", "")
-        list_of_objects.append(price_text)
+        price = soup(text='Ориентировочная cтоимость закупки')
+        text_price = 'Ориентировочная cтоимость закупки'
+    price_tr = price[0].find_parent('div')
+    price_all_span = price_tr.find_all('span')
+    price_index = await index_span(price_all_span, text_price)
+    price_text = price_all_span[price_index + 1].text
+    price_text = price_text.replace("\xa0", "").replace("\\xa", "")
+    list_of_objects.append(price_text)
 
     payer_account_num = soup(text='УНП')
-    payer_account_num_tr = payer_account_num[1].find_parent('tr')
+    payer_account_num_tr = payer_account_num[1].find_parent('div')
     payer_account_num_all_span = payer_account_num_tr.find_all('span')
-    payer_account_num_text = payer_account_num_all_span[1].text
+    payer_account_num_index = await index_span(payer_account_num_all_span, 'УНП')
+    payer_account_num_text = payer_account_num_all_span[payer_account_num_index + 1].text
     list_of_objects.append(payer_account_num_text)
 
     location = soup(text='Место нахождения')
-    location_tr = location[0].find_parent('tr')
+    location_tr = location[0].find_parent('div')
     location_all_span = location_tr.find_all('span')
-    location_text = location_all_span[1].text
+    location_index = await index_span(location_all_span, 'Место нахождения')
+    location_text = location_all_span[location_index].text
     list_of_objects.append(location_text)
 
     return (page, list_of_objects)
